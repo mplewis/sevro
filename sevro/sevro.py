ed8e7b3359451b7f20b40585c7858d7b61945f79
Matt Lewis <matt@mplewis.com>
2020-07-08 14:02:08 -0600

    feat: options hyphenization

--- sevro/sevro.py
+++ sevro/sevro.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 import yaml
 from pprint import pprint
 
@@ -16,6 +18,40 @@ class Config(Model):
     playlists = DictType(StringType)
     youtube_dl_options = ListType(YoutubeDlOption)
 
+    def cli_options(self) -> str:
+        return build_cli_options(standardize_mixed_options(self.youtube_dl_options))
+
+
+def hyphenize(key: str) -> str:
+    """Hyphenize single-char options with one hyphen, all others with two."""
+    if len(key) == 1:
+        return f'-{key}'
+    return f'--{key}'
+
+
+def build_cli_options(opts: dict) -> str:
+    """Convert a dict of CLI options into a CLI option string."""
+    strs = []
+    for key, val in opts.items():
+        with_hyphen = hyphenize(key)
+        if val is None:
+            strs.append(with_hyphen)
+        else:
+            strs.append(f'{with_hyphen}={val}')
+    return ' '.join(strs)
+
+
+def standardize_mixed_options(raw: List[YoutubeDlOption]) -> dict:
+    """Turn [YoutubeDlOption] into a simple key-value dict. Valueless keys are set to value None."""
+    opts = {}
+    for item in raw:
+        if isinstance(item, str):
+            opts[item] = None
+        else:
+            for key, val in item.items():
+                opts[key] = val
+    return opts
+
 
 with open('conf.yaml') as f:
     raw = yaml.safe_load(f)
@@ -23,3 +59,4 @@ with open('conf.yaml') as f:
     conf.validate()
 
 pprint(conf.to_primitive())
+pprint(conf.cli_options())
